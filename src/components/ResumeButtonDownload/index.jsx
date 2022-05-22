9ffc228a0b376c12a8ace7f40d6312a6d84d8224
David Basto <davidbasto01@gmail.com>
2022-05-22 15:34:33 -0500

    [ADD] download resume

--- src/components/ResumeButtonDownload/index.jsx
+++ src/components/ResumeButtonDownload/index.jsx
@@ -1,4 +1,18 @@
 import styles from './index.module.scss';
 export default function ResumeButtonDownload() {
-  return <button className={`primary-button ${styles['Button-download']}`}>Download Resume</button>;
+  const downloadFile = () => {
+    let element = document.createElement('a');
+    element.setAttribute('href', '/David_Basto-resume.pdf');
+    element.setAttribute('target', '_blank');
+    //element.setAttribute('download', '');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+  return (
+    <button onClick={downloadFile} className={`primary-button ${styles['Button-download']}`}>
+      Download Resume
+    </button>
+  );
 }
