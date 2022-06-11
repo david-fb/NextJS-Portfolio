595b0d3506db3e1ad5340e289164ba1930d81f04
David Basto <davidbasto01@gmail.com>
2022-06-10 22:22:12 -0500

    [UPDATE] css class name

--- src/components/SendMessageLoader.jsx
+++ src/components/SendMessageLoader.jsx
@@ -1,7 +1,7 @@
 import styles from '@styles/SendMessageLoader.module.scss';
 export default function SendMessageLoader({ isSending }) {
   return (
-    <div className={styles['Form__loader']}>
+    <div className={styles.container}>
       {isSending ? (
         <>
           <p>Sending message...</p>
@@ -15,7 +15,7 @@ export default function SendMessageLoader({ isSending }) {
       ) : (
         <>
           <p>Message sent</p>
-          <svg className={styles['checkmark']} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+          <svg className={styles.checkmark} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
             <circle className={styles['checkmark__circle']} cx="26" cy="26" r="25" fill="none" />
             <path className={styles['checkmark__check']} fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
           </svg>
